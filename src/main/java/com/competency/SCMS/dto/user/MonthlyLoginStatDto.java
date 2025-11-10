c271c7dacd1e5e44b7211aac9e4d4ffd9836fe37
jsua86268 <jsua86268@gmail.com>
2025-11-10 14:54:13 +0900

    fix: 로그인 오류 해결 및 메일 인증 보안 요소 추가

--- src/main/java/com/competency/SCMS/dto/user/MonthlyLoginStatDto.java
+++ src/main/java/com/competency/SCMS/dto/user/MonthlyLoginStatDto.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MonthlyLoginStatDto {
-
     private Integer year;
     private Integer month;
     private Integer day;
